9e342f6b7acbc511cd1d86ad569fa50062773dcc
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-08-22 10:20:25 +0600

    card complete

--- js/index.js
+++ js/index.js
@@ -15,3 +15,18 @@ document.getElementById('player-2').addEventListener('click', function(){
     let child = countPlayer.childNodes[3];
     child.innerText = player02;
 })
+document.getElementById('player-3').addEventListener('click', function(){
+    let player02 = player('player-3-name');
+    let child = countPlayer.childNodes[5];
+    child.innerText = player02;
+})
+document.getElementById('player-4').addEventListener('click', function(){
+    let player02 = player('player-4-name');
+    let child = countPlayer.childNodes[7];
+    child.innerText = player02;
+})
+document.getElementById('player-5').addEventListener('click', function(){
+    let player02 = player('player-5-name');
+    let child = countPlayer.childNodes[9];
+    child.innerText = player02;
+})

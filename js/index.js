c928f0394e712f5791413cde38ad9ca9f82beb30
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-08-23 05:38:02 +0600

    complete function section

--- js/index.js
+++ js/index.js
@@ -1,32 +1,120 @@
+let count = 0;
 
-function player(elementId){
-    const playerName = document.getElementById(elementId);
-    const playerNameValue = playerName.innerText;
-    return playerNameValue   
+function disabled(disableId){
+   const butonDiabled =  document.getElementById(disableId).disabled = true;
 }
-const countPlayer = document.getElementById('count-player');
+
+
 document.getElementById('player-1').addEventListener('click', function(){
-    let player01 = player('player-1-name');
-    let child =  countPlayer.childNodes[1];
-    child.innerText = player01;  
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-1-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-1');
+   count++;
+    }
 })
 document.getElementById('player-2').addEventListener('click', function(){
-    let player02 = player('player-2-name');
-    let child = countPlayer.childNodes[3];
-    child.innerText = player02;
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-2-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-2');
+   count++;
+    }
 })
 document.getElementById('player-3').addEventListener('click', function(){
-    let player02 = player('player-3-name');
-    let child = countPlayer.childNodes[5];
-    child.innerText = player02;
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-3-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-3');
+   count++;
+    }
 })
 document.getElementById('player-4').addEventListener('click', function(){
-    let player02 = player('player-4-name');
-    let child = countPlayer.childNodes[7];
-    child.innerText = player02;
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-4-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-4');
+   count++;
+    }
 })
 document.getElementById('player-5').addEventListener('click', function(){
-    let player02 = player('player-5-name');
-    let child = countPlayer.childNodes[9];
-    child.innerText = player02;
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-5-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-5');
+   count++;
+    }
 })
+document.getElementById('player-6').addEventListener('click', function(){
+    if(count >4){
+        alert('You have already selected 5 players!');
+    }
+    else{
+    const countPlayer = document.getElementById('count-player');
+    const player = document.getElementById('player-6-name').innerText;
+   const li = document.createElement('li');
+   countPlayer.appendChild(li);
+   li.innerText = player ;
+   disabled('player-6');
+   count++;
+    }
+})
+function inputValue(elementId){
+    const inputField = document.getElementById(elementId);
+    const inputFieldString = inputField.value;
+    const inputFieldValue = parseInt(inputFieldString);
+    return inputFieldValue
+}
+function elementValue(elementId){
+    const elemntField = document.getElementById(elementId);
+    const elementdString = elemntField.innerText;
+    const elementdValue = parseInt(elementdString)
+    return elementdValue
+}
+document.getElementById('budget-calculate').addEventListener('click', function(){
+   const budget = inputValue('budget');
+   const PlayerExpensesString = document.getElementById('player-expenses')
+   const playerCount = budget * count;
+   PlayerExpensesString.innerText = playerCount
+})
+document.getElementById('total-calculate').addEventListener('click', function(){
+    const manager = inputValue('manager');
+    const coach = inputValue('coach');
+    const PlayerExpenses = elementValue('player-expenses');
+    const totalExpenses = manager + coach + PlayerExpenses;
+    const total = document.getElementById('total');
+    total.innerText = totalExpenses
+})
+
+
+
